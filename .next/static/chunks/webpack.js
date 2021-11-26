666929b20b201cba446c3f5f8db704393de9aa05
Olga Mujinga <monaolgita@gmail.com>
2021-11-26 13:06:08 +0100

    refactor: create respensive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e74680e5b8e07a998b4c"; }
+/******/ 		__webpack_require__.h = function() { return "7fc5d3b5ffb630c8411a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
