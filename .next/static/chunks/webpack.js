ebe51bf0704085c61f20f3b8c1fedb4e0fe6221b
Olga Mujinga <monaolgita@gmail.com>
2021-12-02 10:23:46 +0100

    feat: create connexion page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9053ab1ab3a623b658a"; }
+/******/ 		__webpack_require__.h = function() { return "477619aac88cf6e996b2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
